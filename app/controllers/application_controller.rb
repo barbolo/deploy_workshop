4bd661754017a4780d31e297e04d789f12ab982c
Rafael Barbolo <rafael.barbolo@infosimples.com.br>
2014-08-06 00:07:58 -0300

    Add a ping action to test if the application is up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  #
+  # This action can be used by other applications to check if the server
+  # is still up and responding. Currently, it is used in the "health check"
+  # configuration of the Amazon Load Balancer.
+  #
+  def ping
+    render :text => "#{Time.now}: OK"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PaperclipDemo::Application.routes.draw do
   resources :friends
   root :to => 'friends#index'
+
+  get '/ping', to: 'application#ping'
 end
